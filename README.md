7e31116cbc00e7c746d48bd78a8ae22900974b00
sinhahariom <sinhahariom2@gmail.com>
2021-04-28 00:24:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# codebrain-js-throttling
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/codebrain-js-throttling)
---
+++ index.js
@@ -0,0 +1,21 @@
+var i = 0;
+const throttled = () => {
+  console.log("This is throttled..." + ++i);
+};
+
+const implementThrottle = (fn, delay) => {
+  var closureFlag = true;
+  var timer;
+  return function() {
+    //clearInterval(timer);
+    if (closureFlag) {
+      throttled(this, arguments);
+      closureFlag = false;
+      timer = setTimeout(() => {
+        closureFlag = true;
+      }, delay);
+    }
+  };
+};
+const throttleLayer = implementThrottle(throttled, "2000");
+window.addEventListener("resize", throttleLayer);
